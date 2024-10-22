4af53a2a2f80b0cd591bf3c7f4497732baf73dd1
blameitonme1 <2741122036@qq.com>
2024-10-22 11:54:25 +0800

    save workable distill script

--- distill.py
+++ distill.py
@@ -27,34 +27,45 @@ student_model = VMHeadModel(model_config).to(device)
 
 mlp_weights_from_teacher = []
 
-for name, param in teacher_model.named_parameters():
-    if "intermediate" in name or "output" in name:
-        if "bias" not in name and "attention" not in name:
-            # print(f"found mlp in layer {name}")
-            mlp_weights_from_teacher.append(param)
+# for name, param in teacher_model.named_parameters():
+#     if "intermediate" in name or "output" in name:
+#         if "bias" not in name and "attention" not in name:
+#             # print(f"found mlp in layer {name}")
+#             mlp_weights_from_teacher.append(param)
+# show teacher parameter
+param1 = 0
+for n, p in teacher_model.named_parameters():
+    param1 += p.numel()
+param2 = 0
+for n, p in student_model.named_parameters():
+    if p.requires_grad:
+        param2 += p.numel()
+
+print(f"teacher parameters: {param1}")
+print(f"student parameters: {param2}")
 
 student_model.requires_grad_(True)
-layer_idx = 0
-for name, param in student_model.named_parameters():
-    if any(
-        [
-            n in name
-            for n in [
-                "mlp",
-                # "input_layernorm",
-                # "embedding",
-                # "final_layernorm",
-                # "vm_head",
-            ]
-        ]
-    ):
-        param.requires_grad_(False)
-        if "mlp" in name and "bias" not in name:
-            # print(name)
-            param.data = mlp_weights_from_teacher[layer_idx].data
-            layer_idx += 1
-    else:
-        param.requires_grad_(True)
+# layer_idx = 0
+# for name, param in student_model.named_parameters():
+#     if any(
+#         [
+#             n in name
+#             for n in [
+#                 "mlp",
+#                 # "input_layernorm",
+#                 # "embedding",
+#                 # "final_layernorm",
+#                 # "vm_head",
+#             ]
+#         ]
+#     ):
+#         param.requires_grad_(False)
+#         if "mlp" in name and "bias" not in name:
+#             # print(name)
+#             param.data = mlp_weights_from_teacher[layer_idx].data
+#             layer_idx += 1
+#     else:
+#         param.requires_grad_(True)
 
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -65,32 +76,28 @@ transform = transforms.Compose([
 dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
-optimizer = optim.AdamW(filter(lambda p: p.requires_grad, student_model.parameters()), lr=5e-4)
-# criterion_kl = nn.KLDivLoss(reduction="batchmean")
+optimizer = optim.AdamW(student_model.parameters(), lr=1e-3)
+criterion_kl = nn.KLDivLoss(reduction="batchmean")
 criterion_ce = nn.CrossEntropyLoss()
 student_model.train()
 
-# check trainable parameters
-
-# for name, param in student_model.named_parameters():
-#     if param.requires_grad:
-#         print(name)
+# check trainable parameter
 
 for epoch in range(1):
     for (images, labels) in tqdm(dataloader):
         images, labels = images.to(device), labels.to(device, dtype=torch.long)
 
-        # with torch.no_grad():
-        #     teacher_outputs = teacher_model(images).last_hidden_state # logits -> log probabilities
-        #     teacher_outputs = nn.functional.softmax(teacher_outputs, dim=1)
+        with torch.no_grad():
+            teacher_outputs = teacher_model(images).last_hidden_state # logits -> log probabilities
+            teacher_outputs = teacher_outputs[:, 1:, :]
+            teacher_outputs = nn.functional.softmax(teacher_outputs, dim=1)
 
-        student_outputs = student_model(images).logits
-        # student_outputs = nn.functional.log_softmax(student_outputs, dim=1)  # log probabilities
-        # student_outputs = torch.argmax(student_outputs, dim=1)
+        student_outputs = student_model(images).last_hidden_state  # logits -> log probabilities
+        student_outputs = nn.functional.log_softmax(student_outputs, dim=1)  # log probabilities
         # change dtype
-        # print(student_outputs.shape, labels.shape)
+        # print(student_outputs.shape, teacher_outputs.shape)
         # loss_kl = criterion_kl(student_outputs, teacher_outputs)
-        loss_ce = criterion_ce(student_outputs, labels.to(device))
+        loss_ce = criterion_ce(student_outputs, teacher_outputs)
         loss = loss_ce
         sys.stdout.write(f"{loss:.4f}\n")
         optimizer.zero_grad()
